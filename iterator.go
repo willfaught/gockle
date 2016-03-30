66b021464268f71386568122a63720c1bb851e01
Will Faught <will.faught@gmail.com>
2016-03-29 18:54:35 -0700

    Improve Iterator doc

--- iterator.go
+++ iterator.go
@@ -5,17 +5,17 @@ import (
 	"github.com/maraino/go-mock"
 )
 
-// Iterator wraps *gocql.Iter.
+// Iterator iterates CQL query result rows.
 type Iterator interface {
-	// Close closes the iterator.
+	// Close closes the Iterator.
 	Close() error
 
-	// Scan puts the current row in results and returns whether there are more
-	// rows.
+	// Scan puts the current result row in results and returns whether there are
+	// more result rows.
 	Scan(results ...interface{}) bool
 
-	// ScanMap puts the current row in results and returns whether there are
-	// more rows.
+	// ScanMap puts the current result row in results and returns whether there are
+	// more result rows.
 	ScanMap(results map[string]interface{}) bool
 }
 
